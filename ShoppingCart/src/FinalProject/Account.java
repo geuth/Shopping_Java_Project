96812146d54f0be7160fa2427e6cee0bd2c536f4
Geu Thao <geu.thao.f@gmail.com>
2020-03-26 13:20:35 -0500

    added a create new account button

---
+++ ShoppingCart/src/FinalProject/Account.java
@@ -0,0 +1,89 @@
+package FinalProject;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//Final Project Object Oriented Programming
+//By Geu THAO 3/25/20
+
+public class Account extends JFrame implements ActionListener {
+	public static final int WIDTH = 500;
+	public static final int HEIGTH = 400;
+	public static final int NUMBER_OF_CHAR = 30;
+	public static final int NUMBER_OF_CHARACTERS = 30;
+
+	private JTextField username;
+	private JTextField password;
+
+	public static void main(String args[]) {
+		Account gui = new Account();
+
+	}
+
+	public Account() {
+		JFrame shoppingCartFrame = new JFrame("Account Log-in");
+
+		JLabel entryText = new JLabel("Please enter your username and password");
+		entryText.setBounds(70, 90, 280, 30);
+
+		JLabel loginLabel = new JLabel("Username :");
+		loginLabel.setBounds(70, 130, 80, 30);
+
+		JLabel passwordLabel = new JLabel("Password :");
+		passwordLabel.setBounds(70, 170, 80, 30);
+
+		username = new JTextField();
+		username.setBounds(150, 135, 150, 25);
+
+		password = new JTextField();
+		password.setBounds(150, 175, 150, 25);
+
+		
+		JButton logIn = new JButton("Login");
+		logIn.setBounds(165, 220, 60, 30);
+		logIn.addActionListener(this);
+		logIn.setFont(new Font("Times New Roman", Font.BOLD, 10));
+
+		JButton createNewAccount = new JButton("Create \naccount");
+		createNewAccount.setBounds(105, 255, 180, 30);
+		createNewAccount.addActionListener(this);
+		createNewAccount.setBackground(new Color(0,0,0,0));
+		createNewAccount.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+		createNewAccount.setFont(new Font("Times New Roman", Font.BOLD, 11));
+		
+		shoppingCartFrame.add(entryText);
+		shoppingCartFrame.add(loginLabel);
+		shoppingCartFrame.add(passwordLabel);
+		shoppingCartFrame.add(logIn);
+		shoppingCartFrame.add(createNewAccount);
+		shoppingCartFrame.add(username);
+		shoppingCartFrame.add(password);
+		shoppingCartFrame.setSize(400, 400);
+		shoppingCartFrame.setLayout(null);
+		shoppingCartFrame.setVisible(true);
+
+	}
+
+	private Color Color(int i, int j, int k, int l) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+
+		String actionCommand = e.getActionCommand();
+		if (actionCommand == "logIn") {
+			if (username.equals(this.username) && password.equals(this.password)) {
+
+			} else {
+
+			}
+		}
+		if (actionCommand == "Create account") {
+			
+		}
+
+	}
+
+}
